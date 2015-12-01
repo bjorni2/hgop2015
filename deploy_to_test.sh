824680ad4a36993d02f5fcf78a19a2a0d4a6a459
bjorni2 <bjornb13@ru.is>
2015-12-01 12:34:36 +0000

    Test deployment script takes server ip as parameter.

--- deploy_to_test.sh
+++ deploy_to_test.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
+if [ "$1" == "" ]; then
+  echo Usage: "'./deploy_to_test.sh <SERVER_IP>'"
+  exit 1
+fi
+
+ip="$1"
+
 echo Pushing image to dockerhub..
 docker push bjorni/tictactoe
 
+echo
 echo Killing and removing all containers from test env..
-ssh vagrant@192.168.33.10 'docker rm -f $(docker ps -aq)'
+ssh vagrant@$ip 'docker rm -f $(docker ps -aq)'
 
+echo
 echo pulling latest version from dockerhub..
-ssh vagrant@192.168.33.10 'docker pull bjorni/tictactoe'
+ssh vagrant@$ip 'docker pull bjorni/tictactoe'
 
+echo
 echo Starting the container..
-ssh vagrant@192.168.33.10 'docker run -p 9000:8080 -d -e "NODE_ENV=production" bjorni/tictactoe'
+ssh vagrant@$ip 'docker run -p 9000:8080 -d -e "NODE_ENV=production" bjorni/tictactoe'
