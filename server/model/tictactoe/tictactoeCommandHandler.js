af10ccb8cf8e7d947cd1a9d4ee627b42ac18ad2f
bjorni2 <bjornb13@ru.is>
2015-12-03 22:08:48 +0000

    it should not allow placing in occupied cell

--- server/model/tictactoe/tictactoeCommandHandler.js
+++ server/model/tictactoe/tictactoeCommandHandler.js
@@ -1,6 +1,27 @@
 'use strict';
 
 function tictactoeCommandHandler(events){
+  const gameState = {
+    gameId:events[0] && events[0].gameId,
+    state:'active',
+    playerOne:{
+      name:events[0] && events[0].player,
+      side:events[0] && events[0].side
+    },
+    playerTwo:{
+      name:events[1] && events[1].player,
+      side:events[1] && events[1].side
+    },
+    board:[['','',''],['','',''],['','','']]
+  };  
+
+  events.forEach((e) => {
+    if(e.event === 'movePlaced'){
+      const side = (e.player === gameState.playerOne.name ? gameState.playerOne.side : gameState.playerTwo.side);
+      gameState.board[e.x][e.y] = side;
+    }
+  });
+
   return {
     executeCommand: (cmd) => {
       if(cmd.command === 'createGame'){
@@ -32,6 +53,16 @@ function tictactoeCommandHandler(events){
         }];
       }
       else if(cmd.command === 'placeMove'){
+        if(gameState.board[cmd.x][cmd.y] !== ''){
+          return[{
+            gameId:cmd.gameId,
+            commandId:cmd.commandId,
+            event:'illegalMove',
+            player:cmd.player,
+            timeStamp:cmd.timeStamp
+          }];
+        }        
+
         return[{
           gameId:cmd.gameId,
           commandId:cmd.commandId,
--- server/model/tictactoe/whenPlaceMove.spec.js
+++ server/model/tictactoe/whenPlaceMove.spec.js
@@ -48,4 +48,53 @@ describe('place move command', () => {
     JSON.stringify(actual).should.be.exactly(JSON.stringify(then));
   });
 
+  it('should not allow placing in occupied cell', () => {        
+    given = [{
+      gameId:'1',
+      commandId:'aB12',
+      event:'gameCreated',
+      player:'Jon',
+      side:'X',
+      timeStamp:'2015-12-04T00:00:03Z'
+    },
+    {
+      gameId:'1',
+      commandId:'1337',
+      event:'gameJoined',
+      player:'Bjorn',
+      side:'O',
+      timeStamp:'2015-12-04T00:00:30Z'
+    },
+    {
+      gameId:'1',
+      commandId:'blah',
+      event:'movePlaced',
+      player:'Jon',
+      x:0,
+      y:1,
+      timeStamp:'2015-12-04T00:00:45Z'
+    }];
+    
+    when = {
+      gameId:'1',
+      commandId:'1234',
+      command:'placeMove',
+      player:'Bjorn',
+      x:0,
+      y:1,
+      timeStamp:'2015-12-04T00:01:00Z'
+    }
+    
+    then = [{
+      gameId:'1',
+      commandId:'1234',
+      event:'illegalMove',
+      player:'Bjorn',
+      timeStamp:'2015-12-04T00:01:00Z'
+    }];
+
+    let actual = tictactoeCommandHandler(given).executeCommand(when);
+
+    JSON.stringify(actual).should.be.exactly(JSON.stringify(then));
+  });
 });
