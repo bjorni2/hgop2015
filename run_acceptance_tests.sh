765b7145cb42d2aa4f9634bf9395da3f07efeca9
bjorni2 <bjornb13@ru.is>
2015-12-08 21:39:47 +0000

    Added acceptance test file and script

---
+++ run_acceptance_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+if [ "$1" == "" ]; then
+  echo Usage: "'./run_acceptance_tests.sh <API_ADDRESS>'"
+  exit 1
+fi
+
+api="$1"
+
+echo Exporting ACCEPTANCE_URL..
+export ACCEPTANCE_URL=http://$api
+
+echo
+echo Running acceptance tests..
+grunt mochaTest:acceptance
