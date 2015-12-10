a526f5b1d51a24b100755935be332a74f580bb12
bjorni2 <bjornb13@ru.is>
2015-12-10 02:03:04 +0000

    first version of fluid acceptance test + updated should version

--- server/acceptance/tictactoe.acceptance.js
+++ server/acceptance/tictactoe.acceptance.js
@@ -2,6 +2,7 @@
 
 var should = require('should');
 var request = require('supertest');
+var assert = require('assert');
 var acceptanceUrl = process.env.ACCEPTANCE_URL;
 
 
@@ -13,7 +14,7 @@ describe('TEST ENV GET /api/gameHistory', function () {
 
   it('should execute same test using old style', function (done) {
 
-    var command =     {
+    var command = {
       commandId : '1234',
       gameId : '999',
       command: 'createGame',
@@ -35,6 +36,7 @@ describe('TEST ENV GET /api/gameHistory', function () {
           .end(function (err, res) {
             if (err) return done(err);
             res.body.should.be.instanceof(Array);
+            assert(res.body[0].side === 'X' || res.body[0].side === 'O');
             res.body[0].side = 'X';
             should(res.body).eql(
               [{
@@ -51,12 +53,69 @@ describe('TEST ENV GET /api/gameHistory', function () {
   });
 
 
-   it('Should execute fluid API test', function (done) {
-     /*
-     given(user('YourUser').createsGame('TheFirstGame'))
-     .expect('GameCreated').withName('TheFirstGame').isOk(done);
-      */
-     done();
-   });
+  it('Should execute fluid API test', function (done) {
+    var command = {
+      commandId : '1234',
+      gameId : '888',
+      command: 'createGame',
+      player: 'Bjorn',
+      timeStamp: '2014-12-02T11:29:29Z'
+    };
+    
+    var gcEvent = {
+      'commandId': '1234',
+      'gameId': '888',
+      'event': 'gameCreated',
+      'player': 'Bjorn',
+      'side': 'X',
+      'timeStamp': '2014-12-02T11:29:29Z'
+    };
+    
+    given(command).sentTo('/api/createGame').expect(gcEvent).when(done);
+  });
 
 });
+
+function given(cmd){
+  var cmd = cmd;
+  var destination = undefined;
+  var expectations = [];
+  
+  var givenApi = {
+    sentTo: function(dest){
+      destination = dest;
+      return givenApi;
+    },
+    expect: function(evnt){
+      expectations.push(evnt);
+      return givenApi;
+    },
+    and: function(evnt){
+      expectations.push(evnt);
+      return givenApi;
+    },
+    when: function(done){
+      var req = request(acceptanceUrl);
+      req
+        .post(destination)
+        .type('json')
+        .send(cmd)
+        .end(function (err, res) {
+          if (err) return done(err);
+          request(acceptanceUrl)
+            .get('/api/gameHistory/' + cmd.gameId)
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function (err, res) {
+              if (err) return done(err);
+              res.body.should.be.instanceof(Array);
+              res.body[0].side.should.be.equalOneOf('X', 'O');
+              res.body[0].side = 'X';
+              should(res.body).eql(expectations);
+              done();
+            });
+        });
+    }
+  }
+  return givenApi;
+}
