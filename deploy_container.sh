44db1c986357ed21b0bfd33813912141277d07c2
bjorni2 <bjornb13@ru.is>
2015-12-14 22:22:15 +0000

    Renamed deployment script and changed it to support more than one target

--- deploy_container.sh
+++ deploy_container.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-set -e
-set -o pipefail
-
 if [ "$1" == "" ]; then
   echo Usage: "'./deploy_to_test.sh <SERVER_IP> <PORT> <TAG>'"
   exit 1
@@ -11,13 +8,16 @@ fi
 ip="$1"
 
 echo
-echo Killing and removing all containers from test env..
+echo Killing and removing container on port $2..
 ssh vagrant@$ip "docker rm -f tictactoe$2"
+
+set -e
+set -o pipefail
 
 echo
 echo pulling latest version from dockerhub..
-ssh vagrant@$ip "docker pull bjorni/tictactoe$2"
+ssh vagrant@$ip "docker pull bjorni/tictactoe:$3"
 
 echo
 echo Starting the container..
-ssh vagrant@$ip "docker run -p $2:8080 -d --name tictactoe$2 -e 'NODE_ENV=production' bjorni/tictactoe$3"
+ssh vagrant@$ip "docker run -p $2:8080 -d --name tictactoe$2 -e 'NODE_ENV=production' bjorni/tictactoe:$3"
