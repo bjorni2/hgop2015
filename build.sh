b42007fa672b85e0484beea78ae50ae6d439fe03
bjorni2 <bjornb13@ru.is>
2015-12-14 15:34:59 +0000

    Gruntfile uses env variable for test output file

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 set -e
 set -o pipefail
 
+if [ -z "$GIT_COMMIT" ]; then
+  export GIT_COMMIT=$(git rev-parse HEAD)
+  export GIT_URL=$(git config --get remote.origin.url)
+fi
+
 export 	DISPLAY=:0
 npm install
 bower install
